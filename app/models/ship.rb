17b78bcad5de162c7ec67f75975e0d52f43b37f2
celestesmthomas <celestesmthomas@gmail.com>
2020-08-08 17:00:27 -0700

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+    attr_reader :name, :type, :booty_attribute
+
+    SHIPS = []
+
+    def initialize(args)
+        @name = args[:name]
+        @type = args[:type]
+        @booty_attribute = args[:booty_attribute]
+        SHIPS << self
+    end
+
+    def self.all
+        COURSES
+    end
+
+    def self.clear
+        COURSES.clear
+    end
 end
